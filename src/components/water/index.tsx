c72efa144e02ad37404f0100d29c8055ddefc3ed
reza sanjari <sanjarireza1996@gmail.com>
2021-09-10 10:04:00 -0700

    first commit

---
+++ src/components/water/index.tsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+import { Button } from 'reactstrap';
+
+
+
+function Water() {
+
+    return (
+       <div className="drops"> 
+       
+       <div className="drop" id="animation"> </div>
+       <div className="drop"> </div>
+       <div className="drop"> </div>
+       <div className="drop"> </div>
+       </div>
+    );
+    
+}
+export default Water;
